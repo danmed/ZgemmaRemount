1bd151407b0269f93011e69865ba9693461779f7
danmed <danmed@gmail.com>
2017-06-21 14:08:08 +0100

    updating logging
    wasn't logging anything previously

--- remount.sh
+++ remount.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if mountpoint -q /mnt/net/recordings
 then
-   echo "mounted"
+   echo "Time: $(date). Already mounted" >> /usr/script/remount.txt
 else
    mount -a >> remount.txt
+   echo "Time: $(date). Had to Remount." >> /usr/script/remount.txt
 fi
+
 
